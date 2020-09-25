5ea245c635742abe207485b901a01f381294ef77
andrewjspivey <12spivey@gmail.com>
2020-09-25 06:33:32 -0700

    last commit added unshift

--- controllers/review.js
+++ controllers/review.js
@@ -48,7 +48,7 @@ router.post("/", async (req, res) =>{
         const createdReview = await db.Review.create(req.body);
         const foundRestaurant = await db.Restaurant.findById(req.body.restaurant);
     
-        await foundRestaurant.review.push(createdReview);
+        await foundRestaurant.review.unshift(createdReview);
         await foundRestaurant.save();
     
         res.redirect("/restaurants");
